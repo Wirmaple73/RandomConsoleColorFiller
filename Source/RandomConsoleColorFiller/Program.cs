4b9310d57738667d21d88c40c39060407c30f5ea
Wirmaple73 <wirmaple73@gmail.com>
2023-02-22 20:02:47 +0330

    Add files via upload

---
+++ Source/RandomConsoleColorFiller/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandomConsoleColorFiller
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Random Console Color Filler";
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                ConsoleColorFiller.FillScreen();
+
+                Console.SetCursorPosition(0, 0);
+                Console.ReadKey(true);
+            }
+        }
+    }
+
+    static class ConsoleColorFiller
+    {
+        static Random rnd = new Random();
+        const short numCharsToFill = 1000;
+
+        public static void FillScreen()
+        {
+            for (short i = 0; i < numCharsToFill; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(0, 16);
+                Console.Write("██");
+            }
+        }
+    }
+}
